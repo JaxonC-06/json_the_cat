568c63c9f436a77fce9b283e2be2b3f7d9edb7f8
JaxonC-06 <jaxoncostain@outlook.com>
2025-03-23 15:19:22 -0600

    Added working test files for Mocha and Chai

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,17 @@
 const needle = require('needle');
-const breed = process.argv[2];
 
-needle.get(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
-  if (error) throw "There was a problem accessing the URL";
+const fetchBreedDescription = function(breed, callback) {
+  needle.get(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
+    if (error) {
+      return callback(error, null);
+    }
 
-  if (body.length === 0) {
-    console.log(`The breed ${breed} could not be found. Please verify the existence or spelling of the cat breed: ${breed}`);
-    return;
-  }
+    if (body.length === 0) {
+      return callback("breed not found", null);
+    }
 
-  if (!error && response.statusCode === 200) {
-    console.log(body[0].description);
-  }
-});
+    callback(null, body[0].description);
+  });
+};
+
+module.exports = { fetchBreedDescription };
